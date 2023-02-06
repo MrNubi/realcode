5a271437220a6152bd6c6528e2484045fd5aa51e
jahun <99630193+MrNubi@users.noreply.github.com>
2023-02-07 00:34:54 +0900

    0207 버그 최종수정 커밋

--- pages/Channel/index.tsx
+++ pages/Channel/index.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import Workspace12 from '../../wk/workspace12';
-const Channel = () => {
-  return (
-    <Workspace12>
-      <div>로그인 완료</div>;
-    </Workspace12>
-  );
+
+const channel = () => {
+  return <div>"dsd"</div>;
 };
 
-export default Channel;
+export default channel;
