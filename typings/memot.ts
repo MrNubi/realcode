8b511742870a3c82d4b379341b09339ceccc6f92
jahun <99630193+MrNubi@users.noreply.github.com>
2023-02-19 22:15:07 +0900

    css수정

--- typings/memot.ts
+++ typings/memot.ts
@@ -1,15 +1,15 @@
 export interface MLogin {
-  access_token:string
-  refresh_token:string
-  user:MUSer[]
+  access_token: string;
+  refresh_token: string;
+  user: MUSer[];
 }
 
-export interface MUSer{
-  pk: number,
-  username: string,
-  email: string,
-  first_name: string,
-  last_name: string,
-  nickname: string,
-  avatar: string | null
+export interface MUSer {
+  pk: number;
+  username: string;
+  email: string;
+  first_name: string;
+  last_name: string;
+  nickname: string;
+  avatar: string | null;
 }
